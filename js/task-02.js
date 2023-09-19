d0ff11033c40556404677f1714c2b70ee78ad69d
AmmelyStar <113164996+AmmelyStar@users.noreply.github.com>
2023-09-19 20:40:38 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,23 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const list = document.querySelector('ul#ingredients');
+
+const li = document.createElement('li');
+
+const markup = ingredients.map((ingredient) => {
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  li.classList.add('item');
+  list.append(li);
+  return li;
+
+});
+
+ console.log(list);
+
+
+
+
