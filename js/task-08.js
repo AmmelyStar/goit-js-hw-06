675bc47547f640e2c11c6ca07fd01728b09119e5
AmmelyStar <113164996+AmmelyStar@users.noreply.github.com>
2023-09-24 17:32:04 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const inputForm = document.querySelector('.login-form');
 
+inputForm.addEventListener('submit', loginForm);
+
+function loginForm(event) {
+    event.preventDefault();
+    const { email, password } = event.currentTarget.elements;
+
+    if (email.value === '' || password.value === '') {
+        alert(`Будь ласка заповніть усі поля`);
+    };
+   
+    const data = {
+        email: email.value,
+        password: password.value
+    }
+
+    console.log(data);
+
+    event.currentTarget.reset();
+}
